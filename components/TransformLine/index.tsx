a23209906a7b174435d92be5e034782551016013
Sotiris <sp@developgreece.com>
2023-03-11 12:50:21 +0200

    more animation

--- components/TransformLine/index.tsx
+++ components/TransformLine/index.tsx
@@ -9,7 +9,7 @@ export default function TransformLine({ transformation }: Props) {
     if (!transformation) return null
     return (
         <div
-            className="bg-black fixed z-2 transition-all duration-300"
+            className="bg-black fixed z-2 transition-all delay-200 duration-700"
             style={transformation}
         />
     )
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,20 @@
 import Header from '@/components/Header'
-import TransformLine from '@/components/TransformLine' 
-import PersistentElementsContext, {  LineTransformData, PersistentElementsPayload } from '@/contexts/persistent-elements'
- 
+import TransformLine from '@/components/TransformLine'
+import PersistentElementsContext, { LineTransformData, PersistentElementsPayload } from '@/contexts/persistent-elements'
+
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 import Router from "next/router"
-import { useState } from 'react'
-
+import { useEffect, useState } from 'react'
+import noise from '../public/noise.png'
 export default function App({ Component, pageProps }: AppProps) {
 
   const [transformation, setTransformation] = useState<LineTransformData>()
-  
+
   const [bg, setBg] = useState<string>(pageProps.defaultBg || "bg-backGray")
+
+  const [opacity, setOpacity] = useState<string>("opacity-1")
 
   const lineTrPayload: PersistentElementsPayload = {
     setLineData: setTransformation,
@@ -27,13 +29,36 @@ export default function App({ Component, pageProps }: AppProps) {
     },
   }
 
-  return <div> 
-    <div className={`${bg} transition-all duration-300 fixed z-[-1] top-0 left-0 w-[100vw] h-[100vh]`} /> 
-      <TransformLine transformation={transformation} />
-      <PersistentElementsContext.Provider value={lineTrPayload}>
-        <Header />
+  useEffect(() => {
+
+    function dimOpacity() {
+      setOpacity("opacity-0")
+    }
+
+    function resetOpacity() {
+      setTimeout(() => {
+      
+        setOpacity("opacity-1")
+      }, 800);
+    }
+
+    Router.events.on("routeChangeStart", dimOpacity)
+    Router.events.on("routeChangeComplete", resetOpacity)
+    return () => {
+      Router.events.off("routeChangeStart", dimOpacity)
+      Router.events.off("routeChangeComplete", resetOpacity)
+    }
+  }, [])
+
+  return <div>
+    <div style={{backgroundImage: `url(${noise.src})`}} className={`${bg} transition-colors  duration-500 fixed z-[-1] top-0 left-0 w-[100vw] h-[100vh]`} />
+    <TransformLine transformation={transformation} />
+    <PersistentElementsContext.Provider value={lineTrPayload}>
+      <Header />
+      <div className={`${opacity} transition-opacity duration-700`}>
         <Component {...pageProps} />
-      </PersistentElementsContext.Provider> 
+      </div>
+    </PersistentElementsContext.Provider>
   </div>
 
 }
